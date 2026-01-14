a26b818003035f59ed4257b8994665688ef31ca7
Randall McGrew <rrmcgrew@gmail.com>
2026-01-14 13:12:53 -0600

    adds array spread and destruct

---
+++ fundamentals/17-array-spread-destruct.ts
@@ -0,0 +1,7 @@
+const numbers = [1, 2, 3];
+// array spread
+const moreNumbers = [...numbers, 4, 5, 6];
+console.log(moreNumbers);
+// array destruct
+const [first, second, third] = moreNumbers;
+console.log(first, second, third);
